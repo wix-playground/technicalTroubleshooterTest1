4a2cbfd951aee4613529ae29ec0a5486bee9aba5
Michael Salaverry <michaels@wix.com>
2017-01-17 19:22:34 +0200

    for trace route to mac

--- tracerouteMac.sh
+++ tracerouteMac.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-echo $1
-traceroute $1
+echo "Starting traceroutes for domain ${1}"
+function trrt {
+	echo "running traceroute on ${1} and appending to file trrt.txt" | tee -a trrt.txt
+	traceroute -w 3 -q 2 -m 10 $1 >> trrt.txt
+	printf "\n"
+}
+trrt $1
+trrt static.wixstatic.com
+trrt files.wix.com
+trrt static.parastorage.com
+less ./trrt.txt
